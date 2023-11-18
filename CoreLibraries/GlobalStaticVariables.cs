375ae02afce58179dda4bdcc360505db78775528
York393098870 <122362831+York393098870@users.noreply.github.com>
2023-11-19 01:09:45 +0800

    移动旧版本
    旧版本（WPF版本已经移动至仓库）。

---
+++ CoreLibraries/GlobalStaticVariables.cs
@@ -0,0 +1,8 @@
+﻿namespace CoreLibraries;
+
+public static class GlobalStaticVariables
+{
+    public const string UrlGithub = @"https://github.com/York393098870/SR_GachaCalc";
+    public const double GetLimitedCharacterRate = 0.5;
+    public const double GetLimitedWeaponRate = 0.75;
+}
